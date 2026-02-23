c9203857d4e018a5d066ea0f94d84b08504d0016
German <german@german.com>
2026-02-23 17:04:11 +0300

    Добавление тестовых даттых в классе TestData

---
+++ CalcSanatoriumBooking/Data/PriceBooking.cs
@@ -0,0 +1,76 @@
+﻿
+namespace CalcSanatoriumBooking.Data
+{
+	/// <summary>Стоимость бронирования</summary>
+	public class PriceBooking
+	{
+		/// <summary>Дата актуальности стоимости бронирования</summary>
+		private DateOnly _datePriceActual = default;
+
+		/// <summary>Дата актуальности стоимости бронирования</summary>
+		public DateOnly DatePriceActual
+		{
+			get { return _datePriceActual; }
+			set { _datePriceActual = value; }
+		}
+
+		/// <summary>Дата начала действия стоимости бронирования</summary>
+		private DateOnly _startDatePrice = default;
+
+		/// <summary>Дата начала действия стоимости бронирования</summary>
+		public DateOnly StartDatePrice
+		{
+			get { return _startDatePrice; }
+			set { _startDatePrice = value; }
+		}
+
+		/// <summary>Дата окончания действия стоимости бронирования</summary>
+		private DateOnly _endDatePrice = default;
+
+		/// <summary>Дата окончания действия стоимости бронирования</summary>
+		public DateOnly EndDatePrice
+		{
+			get { return _endDatePrice; }
+			set { _endDatePrice = value; }
+		}
+
+		/// <summary>Cтоимость бронирования</summary>
+		private Int32 _currentPriceBooking = default;
+
+		/// <summary>Cтоимость бронирования</summary>
+		public Int32 CurrentPriceBooking
+		{
+			get { return _currentPriceBooking; }
+			set { _currentPriceBooking = value; }
+		}
+
+		/// <summary>Индекс расчета бронирования(индекс: санатория; категория номера, вид размещения)</summary>
+		private String _indexBookingCategory = String.Empty;
+
+		/// <summary>Индекс расчета бронирования</summary>
+		public String IndexBookingCategory
+		{
+			get { return _indexBookingCategory; }
+			set { _indexBookingCategory = value; }
+		}
+
+		/// <summary>Стоимость бронирования</summary>
+		/// <param name="indexBookingCategory">Индекс расчета бронирования(индекс: санатория; категория номера, вид размещения, лечение)</param>
+		/// <param name="datePriceActual">Дата актуальности стоимости бронирования</param>
+		/// <param name="startDatePrice">Дата начала действия стоимости бронирования</param>
+		/// <param name="endDatePrice">Дата окончания действия стоимости бронирования</param>
+		/// <param name="currentPriceBooking">Cтоимость бронирования</param>
+		public PriceBooking(String indexBookingCategory,
+							DateOnly datePriceActual,
+							DateOnly startDatePrice,
+							DateOnly endDatePrice,
+							Int32 currentPriceBooking)
+		{
+			IndexBookingCategory = indexBookingCategory;
+			DatePriceActual = datePriceActual;
+			StartDatePrice = startDatePrice;
+			EndDatePrice = endDatePrice;
+			CurrentPriceBooking = currentPriceBooking;
+		}
+	}
+}
---
+++ CalcSanatoriumBooking/Model/TestData.cs
@@ -0,0 +1,63 @@
+﻿using CalcSanatoriumBooking.Data;
+
+namespace CalcSanatoriumBooking.Model
+{
+	/// <summary>Тестовые данные </summary>
+	public class TestData
+	{
+		/// <summary>Список стоиместей бронирования </summary>
+		private List<PriceBooking>? _priceBookingList = default;
+
+		/// <summary>Список стоиместей бронирования </summary>
+		public List<PriceBooking> PriceBookingList
+		{
+			get { return _priceBookingList!; }
+			set { _priceBookingList = value; }
+		}
+
+		public TestData()
+		{
+			LoadPriceBookingList();
+
+		}
+
+		/// <summary>Заполнить список стоиместей бронирования </summary>
+		public void LoadPriceBookingList()
+		{
+			PriceBooking currentPriceBooking = new PriceBooking("1-1-1-1",
+																DateOnly.Parse("2025,11,17"),
+																DateOnly.Parse("2026, 10, 01"),
+																DateOnly.Parse("2026, 10, 31"),
+																6315);
+			PriceBookingList.Add(currentPriceBooking);
+
+			currentPriceBooking.IndexBookingCategory = "1-1-1-1";
+			currentPriceBooking.DatePriceActual = DateOnly.Parse("2025,11,17");
+			currentPriceBooking.StartDatePrice = DateOnly.Parse("2026, 11, 01");
+			currentPriceBooking.EndDatePrice = DateOnly.Parse("2026, 12, 31");
+			currentPriceBooking.CurrentPriceBooking = 5600;
+
+			PriceBookingList.Add(currentPriceBooking);
+
+			currentPriceBooking.IndexBookingCategory = "1-1-1-1";
+			currentPriceBooking.DatePriceActual = DateOnly.Parse("2026,08,01");
+			currentPriceBooking.StartDatePrice = DateOnly.Parse("2026, 10, 01");
+			currentPriceBooking.EndDatePrice = DateOnly.Parse("2026, 10, 31");
+			currentPriceBooking.CurrentPriceBooking = 5490;
+
+			PriceBookingList.Add(currentPriceBooking);
+
+			currentPriceBooking.IndexBookingCategory = "1-1-1-1";
+			currentPriceBooking.DatePriceActual = DateOnly.Parse("2026,08,01");
+			currentPriceBooking.StartDatePrice = DateOnly.Parse("2026, 11, 01");
+			currentPriceBooking.EndDatePrice = DateOnly.Parse("2026, 12, 31");
+			currentPriceBooking.CurrentPriceBooking = 4870;
+
+			PriceBookingList.Add(currentPriceBooking);
+
+
+
+		}
+
+	}
+}
