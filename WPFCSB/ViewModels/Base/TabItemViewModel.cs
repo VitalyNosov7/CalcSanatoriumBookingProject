77e36cf6404d017ea0e409052d4d25d279b1e59f
Vitaly <vitaly@example.com>
2026-05-15 15:54:21 +0300

    Реализация интерфейса ICommand

--- WPFCSB/ViewModels/Base/TabItemViewModel.cs
+++ WPFCSB/ViewModels/Base/TabItemViewModel.cs
@@ -1,15 +1,32 @@
 ﻿
 namespace WPFCSB.ViewModels.Base
 {
+	// TODO: подумать, нужен ли этот класс(усложняю код)
+
     public class TabItemViewModel
     {
-		private String _header = String.Empty;
+		private Dictionary<Object,String> _header = default;
 
-		public String Header
+		public Dictionary<Object, String> Header
 		{
 			get { return _header; }
 			set { _header  = value; }
 		}
+
+		private String _content = String.Empty;
+
+		public String Content
+		{
+			get { return _content = String.Empty; }
+			set { _content = value; }
+		}
+
+		/// <summary>Добавить элемент в TabItem.Header</summary>
+		public void AddElemenToHeader()
+		{
+
+		}
+
 
 	}
 }
--- WPFCSB/ViewModels/TabBookingViewModel.cs
+++ WPFCSB/ViewModels/TabBookingViewModel.cs
@@ -1,8 +1,49 @@
 ﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using WPFCSB.ViewModels.Base;
+
 namespace WPFCSB.ViewModels
 {
     public class TabBookingViewModel
     {
+        TabControl tabControl = new TabControl();
 
+        private void AddTabItem(object sender, RoutedEventArgs e)
+        {
+            TabItem tabItem = new TabItem();
+            StackPanel stackPanelTabItemHeader = new StackPanel();
+            // stackPanel.Name = "currentStackPanel";
+            Ellipse ellipse = new Ellipse() { Height = 10, Width = 10, Fill=Brushes.Black};
+            TextBlock textBlock = new TextBlock() { Margin = new Thickness(3), Text = "Программный Таб" };
+
+            stackPanelTabItemHeader.Orientation = Orientation.Horizontal;
+            stackPanelTabItemHeader.Children.Add(ellipse);
+            stackPanelTabItemHeader.Children.Add(textBlock);
+
+            tabItem.Header = stackPanelTabItemHeader;
+
+            tabControl.Visibility = Visibility.Visible;
+            tabControl.Items.Add(tabItem);
+            tabItem.IsSelected = true;
+        }
+
+        private void DelTabItem(object sender, RoutedEventArgs e)
+        {
+            //products.Items.RemoveAt(products.SelectedIndex);
+
+            //TabItem current = (TabItem)tabControl.SelectedItem;
+            //current.Visibility = Visibility.Collapsed;
+            //tabItemStart.Visibility = Visibility.Collapsed;
+            //// tabControl.Visibility = Visibility.Collapsed;
+            //stackPanelTabItemHeader.Visibility = Visibility.Collapsed;
+        }
+
+        public TabBookingViewModel()
+        {
+         
+        }
     }
 }
