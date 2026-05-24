ebf823f9f877b4617c9d006acd177c8193fea00f
German <german@german.com>
2026-05-24 18:58:08 +0300

    Добавлена модель TabItemModel

---
+++ WPFCSB/Models/TabItemModel.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System.Collections.ObjectModel;
+
+namespace WPFCSB.Models
+{
+	public class TabItemModel
+	{
+		/// <summary>Заголовок вкладки</summary>
+		private ObservableCollection<Object>? _tabItemHeader;
+
+		/// <summary>Заголовок вкладки</summary>
+		public ObservableCollection<Object> TabItemHeader
+		{
+			get { return _tabItemHeader!; }
+			set { _tabItemHeader = value; }
+		}
+
+		/// <summary>Заголовок содержимого вкладки</summary>
+		private ObservableCollection<Object>? _tabItemContent;
+
+		/// <summary>Заголовок содержимого вкладки</summary>
+		public ObservableCollection<Object>? TabItemContent
+		{
+			get { return _tabItemContent!; }
+			set { _tabItemContent = value; }
+		}
+
+		public void AddTabItemHeader(Object  tabItemHeader)
+		{
+			
+		}
+
+	}
+}
